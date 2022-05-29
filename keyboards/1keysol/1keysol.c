05b9df4e5b4f58a3d662b64147129ee1aeb58750
hRake <hrake5857@gmail.com>
2022-05-29 10:47:10 -0400

    1keysol 1st commit
    base setup for first test

---
+++ keyboards/1keysol/1keysol.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <quantum.h>
+#include <hal_pal.h>
+
+void keyboard_post_init_kb(void)
+{
+    wait_ms(5000);
+    setPinOutput(B13);
+    writePin(B13, 1);
+    wait_ms(4);
+    writePin(B13,0);
+    //debug_enable=true;
+    //debug_matrix=true;
+}
